fba3ecc89abbe49cd9c40a6db6fd15e085b56fe9
Paul Leavitt <pleavitt@gmail.com>
2018-08-10 22:33:00 +1000

    Completed all endpoints

--- src/main/java/pleavitt/math/api/MathController.java
+++ src/main/java/pleavitt/math/api/MathController.java
@@ -8,22 +8,47 @@ import org.springframework.web.bind.annotation.RestController;
 public class MathController {
 
     @GetMapping("/add")
-    public Result doAdd(@RequestParam float op1, @RequestParam float op2){
+    public Result doAdd(@RequestParam float op1, @RequestParam float op2) {
         return new Result(op1 + op2);
     }
 
     @GetMapping("/subtract")
-    public Result doSubtract(@RequestParam float op1, @RequestParam float op2){
-        return new Result(op1 + op2);
+    public Result doSubtract(@RequestParam float op1, @RequestParam float op2) {
+        return new Result(op1 - op2);
     }
 
     @GetMapping("/multiply")
-    public Result doMultiply(@RequestParam float op1, @RequestParam float op2){
-        return new Result(op1 + op2);
+    public Result doMultiply(@RequestParam float op1, @RequestParam float op2) {
+        return new Result(op1 * op2);
     }
 
     @GetMapping("/divide")
-    public Result doDivide(@RequestParam float op1, @RequestParam float op2){
-        return new Result(op1 + op2);
+    public Result doDivide(@RequestParam float op1, @RequestParam float op2) {
+        return new Result(op1 / op2);
+    }
+
+    @GetMapping("/power")
+    public Result doPower(@RequestParam float op1, @RequestParam float op2) {
+        return new Result((float) Math.pow(op1, op2));
+    }
+
+    @GetMapping("/square_root")
+    public Result doSquareRoot(@RequestParam float op1) {
+        return new Result((float) Math.sqrt(op1));
+    }
+
+    @GetMapping("/log10")
+    public Result doLog10(@RequestParam float op1) {
+        return new Result((float) Math.log10(op1));
+    }
+
+    @GetMapping("/ln")
+    public Result doLogNatural(@RequestParam float op1) {
+        return new Result((float) Math.log(op1));
+    }
+
+    @GetMapping("/pi")
+    public Result doPi() {
+        return new Result((float) Math.PI);
     }
 }
